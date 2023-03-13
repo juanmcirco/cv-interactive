700b5f1daf85e2790751a141c14d8384704b1f08
Manu Barreto <juanmcirco@gmail.com>
2023-03-13 16:50:34 -0300

    gtm

--- pages/index.js
+++ pages/index.js
@@ -57,6 +57,7 @@ export default function Home({ passEnv }) {
       <Head>
         <title>CV Manu Barreto</title>
         <script async type='text/javascript' src="/js/newRelic.js"></script>
+        <script async type='text/javascript' src="/js/gtm.js"></script>
         <link rel="icon" type="image/x-icon" href="/favicon.png"></link>
       </Head>
 
