d7aae7cf769154a893299f62cad3d036664d3c26
Manu Barreto <juanmcirco@gmail.com>
2023-03-13 18:16:07 -0300

    fix gtm

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import "../styles/global.css";
-import { usePagesViews } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }) {
-    usePagesViews()
     return (
         <Component {...pageProps} />
     );
